4da0c9acac3b8f200c93a84435da830de120aaa2
Dan Stronge <dnlstronge@gmail.com>
2023-05-04 08:32:55 +0100

    [x] - move test into test suite

--- src/components/Greeting.test.js
+++ src/components/Greeting.test.js
@@ -1,16 +1,22 @@
 import { render, screen } from "@testing-library/react"
 import Greeting from "./Greeting"
 
-test("checks text is present", () => {
-    // arrange
-    render(<Greeting/>)
-    // act
-    // ...
+describe("Checks greeting component", () => {
+    test("checks text is present", () => {
+        // arrange
+        render(<Greeting/>)
+        // act
+        // ...
+    
+        // assert
+       const helloWorldElement = screen.getByText("Hello world");
+       expect(helloWorldElement).toBeInTheDocument()
+    })
 
-    // assert
-   const helloWorldElement = screen.getByText("Hello world");
-   expect(helloWorldElement).toBeInTheDocument()
+
 })
+
+
 
 
 /*
