32f584415d01b51e5dcbb2a291109593ffbf8712
Dan Stronge <dnlstronge@gmail.com>
2023-05-05 08:34:06 +0100

    [x] - import component to app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import Greeting from './components/Greeting';
+import Async from './components/Async';
 
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
         <>
         <Greeting />
         <div>learn react</div>
+        <Async />
         </>
   )
 }
