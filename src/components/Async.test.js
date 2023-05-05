cd50a40085e8d117e3dc84d39af04943d2fef57b
Dan Stronge <dnlstronge@gmail.com>
2023-05-05 10:04:03 +0100

    [x] - add mock functon

--- src/components/Async.test.js
+++ src/components/Async.test.js
@@ -3,7 +3,10 @@ import Async from "./Async"
 
 describe("Tests async component - fetch", () => {
     test("Tests whether posts are render correctly", async () => {
-
+        window.fetch = jest.fn()
+        window.fetch.mockResolvedValueOnce({
+            json: async () => [{id: "p1", title: "first post"}]
+        })
         render(<Async />)
         const listItemsPresent = await screen.findAllByRole("listitem")
         expect(listItemsPresent).not.toHaveLength(0)
