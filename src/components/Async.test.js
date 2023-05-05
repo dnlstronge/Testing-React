ee26b3a0c211a5712cfce03cd53ab1ea0c1f7310
Dan Stronge <dnlstronge@gmail.com>
2023-05-05 09:28:24 +0100

    [x] - make test Async [x] - test now passes

--- src/components/Async.test.js
+++ src/components/Async.test.js
@@ -2,8 +2,10 @@ import { render, screen } from "@testing-library/react"
 import Async from "./Async"
 
 describe("Tests async component - fetch", () => {
-    test("Tests whether posts are rendere correctly", () => {
+    test("Tests whether posts are render correctly", async () => {
 
         render(<Async />)
+        const listItemsPresent = await screen.findAllByRole("listitem")
+        expect(listItemsPresent).not.toHaveLength(0)
     })
 })
